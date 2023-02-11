73261265858530a4f16471d9b1f8758f3bc34ad0
hoang <hoangyl245@gmail.com>
2023-02-11 16:43:13 +0700

    update

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,86 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authApi from "API/authApi";
+import usersApi from "API/usersApi";
+import { toast } from "react-toastify";
+
+export const login = createAsyncThunk("user-login", async (payload) => {
+  try {
+    //  call API to login
+    const data = await authApi.login(payload);
+    // set user to localstorage
+    localStorage.setItem("user", JSON.stringify(data.user));
+    toast.success("Đăng nhập thành công", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+    // return userdata to thunk
+    return data.user;
+  } catch (error) {
+    toast.error(error.message, {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  }
+});
+export const updateUserInfo = createAsyncThunk(
+  "user-update",
+  async (payload) => {
+    try {
+      //  call API to update user info
+      const data = await usersApi.updateUser(payload);
+      // set user to localstorage
+      localStorage.setItem("user", JSON.stringify(data.data.data));
+      localStorage.setItem("limitList", JSON.stringify(10));
+      return data.data.data;
+    } catch (error) {
+      toast.error(error.message, {
+        position: toast.POSITION.TOP_CENTER,
+      });
+    }
+  }
+);
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    curent: JSON.parse(localStorage.getItem("user")) || {},
+    setting: {
+      limitList: JSON.parse(localStorage.getItem("limitList")) || 10,
+    },
+    isLogined: JSON.parse(localStorage.getItem("user") || false),
+  },
+  reducers: {
+    logout(state) {
+      toast.success("Đăng xuất thành công", {
+        position: toast.POSITION.TOP_CENTER,
+      });
+      console.log(state);
+      localStorage.removeItem("user");
+      state.curent = {};
+    },
+    changeSetting(state, action) {
+      if (action.payload?.limitList) {
+        localStorage.setItem("limitList", action.payload.limitList);
+      }
+      state.setting = { ...state.setting, ...action.payload };
+    },
+  },
+  extraReducers: {
+    [updateUserInfo.fulfilled]: (state, action) => {
+      if (action.payload) {
+        state.curent = action.payload;
+      }
+    },
+    [login.fulfilled]: (state, action) => {
+      console.log(action);
+      if (action.payload) {
+        state.curent = action.payload;
+        state.isLogined = true;
+      } else {
+        state.isLogined = false;
+      }
+    },
+  },
+});
+const { actions, reducer } = userSlice;
+export const { logout } = actions;
+export const { changeSetting } = actions;
+export default reducer;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ChakraProvider } from "@chakra-ui/react";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+import theme from "theme/theme";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
 import reportWebVitals from './reportWebVitals';
-import Footer from "./layout/Footer"
-import Header from "./layout/Header"
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Header/>
-    <App />
-    <Footer/>
-  </React.StrictMode>
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ChakraProvider theme={theme}>
+      <App />
+      <ToastContainer autoClose={2000} draggablePercent={60} />
+    </ChakraProvider>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
