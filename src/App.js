7c6e4ab4332da7fcc5765c0ec63cb585a7df67f5
hoang <hoangyl245@gmail.com>
2023-02-13 12:43:43 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
 import React from "react";
 import routes from "./routes";
 import Home from "./pages/home";
-import {
-  HashRouter,
-  Redirect,
-  Route,
-  Switch,
-  BrowserRouter,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import { useSelector } from "react-redux";
 export default function App() {
   const isLogined = useSelector((state) => state.user.isLogined);
   return (
-    <React.StrictMode>
-      <HashRouter>
-        <BrowserRouter basename="">
-          {isLogined ? (
-            <Switch>
-              <Route path={`/hocvien`} component={routes} />
-              <Route path={`/home`} elements={<Home/>} />
-              <Route path={`/auth`} component={routes} />
-            </Switch>
-          ) : (
-            <Switch>
-              <Route path={`/auth`} component={routes} />
-              <Route path={`/home`} elements={<Home/>} />
-              <Redirect from="/" to="/auth" />
-            </Switch>
-          )}
-        </BrowserRouter>
-      </HashRouter>
-    </React.StrictMode>
+      <Router basename="">
+          <Route path={`/home`} element={Home} />
+      </Router>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,22 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { ChakraProvider } from "@chakra-ui/react";
-import { store } from "./app/store";
-import { Provider } from "react-redux";
-import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer } from "react-toastify";
-
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <ChakraProvider>
-      <App />
-      <ToastContainer autoClose={2000} draggablePercent={60} />
-    </ChakraProvider>
-  </Provider>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <Provider store={store}>
+        <App />
+        <ToastContainer autoClose={2000} draggablePercent={60} />
+      </Provider>
+  </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
