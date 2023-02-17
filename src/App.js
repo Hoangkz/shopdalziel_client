a868c9930113b6390c01a32467730ad7555c0a8c
hoang <hoangyl245@gmail.com>
2023-02-17 20:52:08 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import * as React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from './routes';
 import DefaultLayout from './layout/default';
 import { useSelector } from "react-redux";
+import { Fragment } from 'react';
 export default function App() {
   const isLogined = useSelector((state) => state.user.isLogined);
   return (
@@ -12,13 +12,20 @@ export default function App() {
         <Routes>
           {publicRoutes.map((route, index) => {
             const Page = route.element;
-            const Layout = route.layout || DefaultLayout
+            let Layout = DefaultLayout
+            if(route.layout){
+              Layout = route.layout
+            }
+            else if(route.layout===null){
+              Layout = Fragment
+            }
             return <Route key={index}
                           path={route.path}
                           element={
                             <Layout>
                               <Page />
-                            </Layout>}/>
+                            </Layout>
+                          }/>
           })}
         </Routes>
       </Router>
