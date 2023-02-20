8a4d2f3f6a10aa55038d06954012a413069e522b
hoang <hoangyl245@gmail.com>
2023-02-20 12:23:33 +0700

    update

--- src/API/axiosClient.js
+++ src/API/axiosClient.js
@@ -6,5 +6,43 @@ const axiosClient = axios.create({
     "Content-Type": "application/json",
   },
 });
+axiosClient.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
 
+axiosClient.interceptors.response.use(
+  response =>{
+    return response
+  },
+  error => {
+
+    const originalRequest = error.config;
+    const refreshToken = localStorage.getItem('refresh_token');
+
+    if (
+      error.response.status === 401 &&
+      refreshToken &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+      return axiosClient
+        .post('/auth/refresh-token/', { refresh_token: refreshToken })
+        .then(response => {
+          const { access: newToken } = response.data;
+          localStorage.setItem('access_token', newToken);
+          axiosClient.defaults.headers['Authorization'] = `Bearer ${newToken}`;
+          return axiosClient(originalRequest);
+        });
+    }
+    return Promise.reject(error);
+  }
+  
+);
 export default axiosClient;
