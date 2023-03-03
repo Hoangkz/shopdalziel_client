c58fe322318128fb34d276e6f4fc168ed31a4f76
hoang <hoangyl245@gmail.com>
2023-03-04 02:33:53 +0700

    update

--- src/pages/auth/Login/Google.js
+++ src/pages/auth/Login/Google.js
@@ -1,22 +1,22 @@
-import GoogleLogin from 'react-google-login';
+// import GoogleLogin from 'react-google-login';
 export default function Google () {
-  const onSuccess = (response) => {
-    console.log(response)
-  };
+  // const onSuccess = (response) => {
+  //   console.log(response)
+  // };
 
-  const onFailure = (response) => {
-    console.log(response);
-  };
+  // const onFailure = (response) => {
+  //   console.log(response);
+  // };
 
   return (
     <div>
-        <GoogleLogin
+        {/* <GoogleLogin
           clientId="258973467365-5tkd75mps3ktb0a6k8cl4q5gv32u9krp.apps.googleusercontent.com"
           buttonText="Đăng nhập với Google"
           onSuccess={onSuccess}
           onFailure={onFailure}
           cookiePolicy={"single_host_origin"}
-        />
+        /> */}
     </div>
   );
 };
