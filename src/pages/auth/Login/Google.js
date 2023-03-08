e4d655e425511fb574f6bb07a4ae5ce2e2057c35
hoang <hoangyl245@gmail.com>
2023-03-08 15:50:24 +0700

    update_google_new

--- src/pages/auth/Login/Google.js
+++ src/pages/auth/Login/Google.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import GoogleLogin from 'react-google-login';
-export default function Google () {
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+export default function Google() {
   const onSuccess = (response) => {
     console.log(response)
   };
@@ -11,13 +11,34 @@ export default function Google () {
 
   return (
     <div>
+      {/* <GoogleOAuthProvider
+        clientId="258973467365-5tkd75mps3ktb0a6k8cl4q5gv32u9krp.apps.googleusercontent.com"
+      >
+
         <GoogleLogin
           clientId="258973467365-5tkd75mps3ktb0a6k8cl4q5gv32u9krp.apps.googleusercontent.com"
           buttonText="Đăng nhập với Google"
           onSuccess={onSuccess}
           onFailure={onFailure}
-          // cookiePolicy={"single_host_origin"}
+          cookiePolicy={"single_host_origin"}
         />
+      </GoogleOAuthProvider> */}
+      <GoogleOAuthProvider
+          clientId="258973467365-5tkd75mps3ktb0a6k8cl4q5gv32u9krp.apps.googleusercontent.com"
+          auto_select={false}
+
+      >
+        <GoogleLogin
+
+          onSuccess={credentialResponse => {
+            console.log(credentialResponse);
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+          
+        />;
+      </GoogleOAuthProvider>
     </div>
   );
 };
