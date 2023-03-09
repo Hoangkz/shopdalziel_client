1651f2373e9ae4bc2ed5b432362314b004a560b9
hoang <hoangyl245@gmail.com>
2023-03-09 14:03:53 +0700

    update_facebook

--- src/pages/auth/Login/Google.js
+++ src/pages/auth/Login/Google.js
@@ -1,45 +1,18 @@
 import React from 'react';
 import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
-export default function Google() {
-  const onSuccess = (response) => {
-    console.log(response)
-  };
 
-  const onFailure = (response) => {
-    console.log(response);
-  };
-
+const App = () => {
   return (
-    <div>
-      {/* <GoogleOAuthProvider
-        clientId="258973467365-5tkd75mps3ktb0a6k8cl4q5gv32u9krp.apps.googleusercontent.com"
-      >
-
+    <GoogleOAuthProvider clientId="258973467365-5tkd75mps3ktb0a6k8cl4q5gv32u9krp.apps.googleusercontent.com"
+    >
+      <div>
         <GoogleLogin
-          clientId="258973467365-5tkd75mps3ktb0a6k8cl4q5gv32u9krp.apps.googleusercontent.com"
-          buttonText="Đăng nhập với Google"
-          onSuccess={onSuccess}
-          onFailure={onFailure}
-          cookiePolicy={"single_host_origin"}
+          onSuccess={(googleUser) => console.log(googleUser)}
+          onFailure={(error) => console.log(error)}
         />
-      </GoogleOAuthProvider> */}
-      <GoogleOAuthProvider
-          clientId="258973467365-5tkd75mps3ktb0a6k8cl4q5gv32u9krp.apps.googleusercontent.com"
-          auto_select={false}
-
-      >
-        <GoogleLogin
-
-          onSuccess={credentialResponse => {
-            console.log(credentialResponse);
-          }}
-          onError={() => {
-            console.log('Login Failed');
-          }}
-          
-        />;
-      </GoogleOAuthProvider>
-    </div>
+      </div>
+    </GoogleOAuthProvider>
   );
 };
 
+export default App;
