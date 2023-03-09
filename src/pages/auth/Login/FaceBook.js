9c8c90588f1aeff076fe0f399238ae0a501f9e68
hoang <hoangyl245@gmail.com>
2023-03-09 14:45:38 +0700

    uninstall_facebook_login

--- src/pages/auth/Login/FaceBook.js
+++ src/pages/auth/Login/FaceBook.js
@@ -1,8 +1,8 @@
-import { Box, Icon } from '@chakra-ui/react';
-import React from 'react';
-import FacebookLogin  from 'react-facebook-login';
-import { FaFacebook } from 'react-icons/fa';
-import "./facebook.css"
+// import { Box, Icon } from '@chakra-ui/react';
+// import React from 'react';
+// import FacebookLogin  from 'react-facebook-login';
+// import { FaFacebook } from 'react-icons/fa';
+// import "./facebook.css"
 export default function Facebook () {
   function responseFacebook(response) {
     console.log(response);
@@ -10,14 +10,14 @@ export default function Facebook () {
 
   return (
     <div>
-      <FacebookLogin
+      {/* <FacebookLogin
         appId="555833559847846"
         fields="name,email,picture"
         callback={responseFacebook}
         cssClass="facebook"
         icon={<Icon className='iconFacebook' color={"#4c69ba"} as={FaFacebook} />}
         textButton={<Box>Đăng nhập với Facebook</Box>}
-      />
+      /> */}
     </div>
   )
 }
