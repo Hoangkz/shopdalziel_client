d2022c1b2c36cf0a8a93aa76aa162e7cece12b58
hoang <hoangyl245@gmail.com>
2023-03-09 14:04:14 +0700

    update_facebook

--- src/pages/auth/Login/FaceBook.js
+++ src/pages/auth/Login/FaceBook.js
@@ -1,23 +1,23 @@
-// import { Box, Icon } from '@chakra-ui/react';
+import { Box, Icon } from '@chakra-ui/react';
 import React from 'react';
-// import FacebookLogin  from 'react-facebook-login';
-// import { FaFacebook } from 'react-icons/fa';
+import FacebookLogin  from 'react-facebook-login';
+import { FaFacebook } from 'react-icons/fa';
 import "./facebook.css"
 export default function Facebook () {
-  // function responseFacebook(response) {
-  //   console.log(response);
-  // }
+  function responseFacebook(response) {
+    console.log(response);
+  }
 
   return (
     <div>
-      {/* <FacebookLogin
+      <FacebookLogin
         appId="555833559847846"
         fields="name,email,picture"
         callback={responseFacebook}
         cssClass="facebook"
         icon={<Icon className='iconFacebook' color={"#4c69ba"} as={FaFacebook} />}
         textButton={<Box>Đăng nhập với Facebook</Box>}
-      /> */}
+      />
     </div>
   )
 }
