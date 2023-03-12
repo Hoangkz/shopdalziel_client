51dcd3aa549bf88d138cb510d70dabccde385ac4
hoang <hoangyl245@gmail.com>
2023-03-12 22:37:13 +0700

    update

--- src/pages/auth/Login/FaceBook.js
+++ src/pages/auth/Login/FaceBook.js
@@ -1,23 +1,22 @@
 import { Box, Icon } from '@chakra-ui/react';
 import React from 'react';
-import FacebookLogin  from 'react-facebook-login';
 import { FaFacebook } from 'react-icons/fa';
 import "./facebook.css"
 export default function Facebook () {
-  function responseFacebook(response) {
-    console.log(response);
-  }
+  // function responseFacebook(response) {
+  //   console.log(response);
+  // }
 
   return (
     <div>
-      <FacebookLogin
+      {/* <FacebookLogin
         appId="210311564990021"
         fields="name,email,picture"
         callback={responseFacebook}
         cssClass="facebook"
         icon={<Icon className='iconFacebook' color={"#4c69ba"} as={FaFacebook} />}
         textButton={<Box>Đăng nhập với Facebook</Box>}
-      />
+      /> */}
     </div>
   )
 }
--- src/pages/auth/Login/Google.js
+++ src/pages/auth/Login/Google.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+// import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 
 const App = () => {
   return (
-    <GoogleOAuthProvider clientId="258973467365-5tkd75mps3ktb0a6k8cl4q5gv32u9krp.apps.googleusercontent.com"
-    >
-      <div>
-        <GoogleLogin
-          onSuccess={(googleUser) => console.log(googleUser)}
-          onFailure={(error) => console.log(error)}
-        />
-      </div>
-    </GoogleOAuthProvider>
+    <>
+      {/* <GoogleOAuthProvider clientId="258973467365-5tkd75mps3ktb0a6k8cl4q5gv32u9krp.apps.googleusercontent.com"
+      >
+        <div>
+          <GoogleLogin
+            onSuccess={(googleUser) => console.log(googleUser)}
+            onFailure={(error) => console.log(error)}
+          />
+        </div>
+      </GoogleOAuthProvider> */}
+    </>
   );
 };
 
